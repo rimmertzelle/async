86f94c31ad012fc2077e6f78f4cca1506dcc6ca2
Rimmert Zelle <rimmertzelle@192.168.68.110>
2021-05-20 11:30:55 +0200

    removed a solution and updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # async
+
+See this [notion page](https://www.notion.so/mmic/Asynchronous-computing-77fdf4ab3088456aba566d731c38f3a2) for some help.
--- example-2-fetch/assets/js/app.js
+++ example-2-fetch/assets/js/app.js
@@ -7,43 +7,20 @@ const domElement = document.getElementById("starwars");
  * Get data from https://swapi.dev/api/people/1/ using Fetch API
  */
 
-//
-// getSwapiData();
+// function call
+getSwapiData();
 /**
  * Function to get the data from the Swapi API and deliver it to the DOM
  */
-// function getSwapiData() {
-//   fetch("https://swapi.dev/api/people/1/")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       const ul = document.createElement("ul");
-//       ul.innerHTML = `<li>${data.name}</li>`;
-//       domElement.append(ul);
-//     })
-//     .catch((err) => {
-//       console.log("something went wrong", err);
-//     });
-// }
-
-getSwapiData()
-  .then((character) => {
-    const ul = document.createElement("ul");
-    ul.innerHTML = `<li>${character.name}</li>`;
-    domElement.append(ul);
-  })
-  .catch((err) => {
-    console.error("something went wrong", err);
-  });
-
-
-
 function getSwapiData() {
-  return fetch("https://swapi.dev/api/people/1/")
+  fetch("https://swapi.dev/api/people/1/")
     .then((response) => response.json())
     .then((data) => {
-      return data;
+      const ul = document.createElement("ul");
+      ul.innerHTML = `<li>${data.name}</li>`;
+      domElement.append(ul);
     })
     .catch((err) => {
-      return err;
+      console.log("something went wrong", err);
     });
 }
